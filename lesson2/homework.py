49d90bdacf7ad3874164af2825450c4dbbe231b7
林裕嘉 <jerryis0812@gmail.com>
2024-11-10 10:07:23 +0000

    1109homework_complete

--- lesson2/homework.py
+++ lesson2/homework.py
@@ -6,27 +6,26 @@
 
 try:
     height = float(input('請輸入身高(cm):'))
-    print(f'身高:{height}')
     weight = float(input('請輸入體重(kg):'))
-    print(f'體重:{weight}')
 except ValueError:
     print('請輸入數字')
 except Exception:
-    # Exeption 包含所有錯誤, 寫在最後的except
     print('發生錯誤')
 else:
     bmi = weight / (height/100)**2
     print(f'BMI={bmi}')
     if bmi < 18.5:
-        print('過輕')
+        status = '過輕'
     elif bmi < 24 :
-        print('正常')
+        status = '正常'
     elif bmi < 27 :
-        print('過重')
+        status = '過重'
     elif bmi < 30:
-        print('輕度肥胖')
+        status = '輕度肥胖'
     elif bmi < 35:
-        print('中度肥胖')
+        status = '中度肥胖'
     else:
-        print('重度肥胖')
+        status = '重度肥胖'
+    print(f'體重狀態{status}')
+print('程式結束')
 
