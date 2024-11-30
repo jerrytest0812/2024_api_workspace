2d49653ba7dd1f01c3cb38bebea17bb44a0c3a07
林裕嘉 <jerryis0812@gmail.com>
2024-11-30 02:20:43 +0000

    improve homework1123

--- lesson4/homework1123.py
+++ lesson4/homework1123.py
@@ -5,11 +5,10 @@ class BMI():
         self.weight = weight
 
     def get_BMI(self):
-        self.bmi = self.weight / (self.height/100)**2
-        return self.bmi
+        bmi = self.weight / (self.height/100)**2
+        return bmi
 
-    def get_status(self) -> str:
-        bmi = self.get_BMI()
+    def get_status(self,bmi:float) -> str:
         if bmi < 18.5:
             status = '過輕'
         elif bmi < 24 :
@@ -24,15 +23,22 @@ class BMI():
             status = '重度肥胖'
         return status
 
-while True:
-    try:
-        name = str(input("姓名:"))
-        height = float(input('請輸入身高(cm):'))
-        weight = float(input('請輸入體重(kg):'))
-        p1 = BMI(name=name,height=height,weight=weight)
-        break 
-    except Exception:
-        print('輸入格式錯誤,請重新輸入!')
-print(f"您的BMI值是{p1.get_BMI()}\n您的體重{p1.get_status()}")
-print('程式結束')
+def main():
+    while True:
+            try:
+                name = str(input("姓名:"))
+                height = float(input('請輸入身高(cm):'))
+                weight = float(input('請輸入體重(kg):'))
+                p1 = BMI(name=name,height=height,weight=weight)
+                break 
+            except Exception:
+                print('輸入格式錯誤,請重新輸入!')
+    bmi = p1.get_BMI()
+    print(f"您的BMI值是{bmi}")
+    print(f"您的體重{p1.get_status(bmi)}")
+    print('程式結束')
+
+
+if __name__ == '__main__':
+   main()
 
