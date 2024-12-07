dbf23900787473899f07a7c976456c0deff48f3f
林裕嘉 <jerryis0812@gmail.com>
2024-12-07 06:26:34 +0000

    use set() to get sitename without repeat

--- lesson6/lesson6_3-1.py
+++ lesson6/lesson6_3-1.py
@@ -4,7 +4,13 @@ import tools
 
 def main():
     data:list[dict] = tools.get_aqi('aqi.xlsx')
-    pprint(data)
+    sitenames:list = []
+    for item in data:
+        sitenames.append(item['sitename'])
+    sitenames=list(set(sitenames))
+    print(len(sitenames))
+    for name in list(set(sitenames)):
+        print(name)
 
 
 if __name__ == '__main__':
