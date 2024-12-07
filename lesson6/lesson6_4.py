0fb6694c486f6fdfa1785c87f44d7f2ce9e8d039
林裕嘉 <jerryis0812@gmail.com>
2024-12-07 08:14:13 +0000

    select sitename and make a table

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -14,3 +14,13 @@ with st.sidebar:
     )
 
     st.title(f"{add_selectbox}")
+
+
+allData :list[dict]= tools.get_aqi(excel_name='aqi.xlsx')
+
+selected_item:list[dict]=[item for item in allData if item['sitename']== add_selectbox ]
+# selected_item:list[dict]=[]
+# for item in allData:
+#     if item['sitename'] == add_selectbox:
+#         selected_item.append(item)
+st.table(data=selected_item)
