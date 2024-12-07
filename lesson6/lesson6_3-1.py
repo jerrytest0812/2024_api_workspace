f157f66fb22e2358988e69b9d0509f997674f4ab
林裕嘉 <jerryis0812@gmail.com>
2024-12-07 07:04:34 +0000

    save data to new file

--- lesson6/lesson6_3-1.py
+++ lesson6/lesson6_3-1.py
@@ -1,10 +1,23 @@
 from pprint import pprint
+
+import openpyxl.workbook
+import openpyxl.worksheet
 import tools
+import openpyxl
+from openpyxl import load_workbook, Workbook, worksheet
 
 
 def main():
-   sitenames = tools.get_sitenames('aqi.xlsx')
-   print(sitenames)
+    sitenames = tools.get_sitenames('aqi.xlsx')
+    print(sitenames)
+    wb:Workbook = openpyxl.Workbook()
+    sheet:worksheet = wb.active
+    sheet.title = "站點名稱"
+    for idy , name in enumerate(sitenames):
+        print(name)
+        sheet.cell(column=1,row=idy+1, value=name)
+    wb.save('Sitename.xlsx')
+
 
 
 if __name__ == '__main__':
