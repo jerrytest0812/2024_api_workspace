baf55ed50ab3b79f83c62d41ba3acc1978402631
林裕嘉 <jerryis0812@gmail.com>
2024-12-20 15:25:40 +0000

    sync to py

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -4,14 +4,17 @@ from requests.exceptions import RequestException , HTTPError
 from io import StringIO
 from csv import DictReader
 url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
+try:
+    r: Response = requests.request('GET',url)
+    r.raise_for_status()
+except HTTPError as e:
+    print(e)
+except RequestException as e:
+    print(e)
+else:
+    print('Success')
 
-r: Response = requests.request('GET',url)
-
-if r.status_code == 200:
-    print("成功")
     file = StringIO(r.text)
     reader = DictReader(file)
     list_reader:list[dict] = list(reader)
     print(list_reader)
-else:
-    print("失敗")
