5c0e371a1641751fa1e6ce0a1e4f26c90b2d2404
林裕嘉 <jerryis0812@gmail.com>
2024-12-21 02:52:43 +0000

    format

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -9,5 +9,9 @@ def increment_counter(**kwargs):
     st.session_state.count -= kwargs['second'] 
 
 st.header(f'count { st.session_state.count}')
-button_status = st.button('again', key='reset',help='+1+1+1',on_click=increment_counter,kwargs={'first':5,'second':3}) #  key='reset' default in button
+button_status = st.button('again',
+                           key='reset',
+                           help='+1+1+1',
+                           on_click=increment_counter,
+                           kwargs={'first':5,'second':3}) #  key='reset' default in button
 st.write(st.session_state)
