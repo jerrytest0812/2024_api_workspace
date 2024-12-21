1c0c51f908c0e68123ff751d2c96e8b31c1d52c8
林裕嘉 <jerryis0812@gmail.com>
2024-12-21 02:24:02 +0000

    button para

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 
 if 'count' not in st.session_state:
-    st.session_state.count=0
+    st.session_state.count=1
 
+def increment_counter(*args):
+    st.session_state.count += args[0] 
+    st.session_state.count -= args[1]
 
 st.header(f'count { st.session_state.count}')
-button_status = st.button('again', key='reset') #  key='reset' default in button
+button_status = st.button('again', key='reset',help='+1+1+1',on_click=increment_counter,args=(5,3)) #  key='reset' default in button
 st.write(st.session_state)
-if button_status:
-    st.session_state.count += 1
